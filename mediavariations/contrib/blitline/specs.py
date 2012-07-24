b830e8d6686981aee434e5150dd3548769f066af
Simon Schmid <ssc@feinheit.ch>
2012-07-24 20:56:49 +0200

    move spec instance meta info into super spec for easy overwriting

--- mediavariations/contrib/blitline/specs.py
+++ mediavariations/contrib/blitline/specs.py
@@ -2,7 +2,6 @@ import os
 import urllib
 
 from django.conf import settings
-from django.core.files.storage import get_storage_class
 from django.utils import simplejson
 
 from ...specs import Base
@@ -18,27 +17,20 @@ class Generic(Base):
 
     def process(self):
         options = self.get_options()
-        original = getattr(self.variation.content_object, self.variation.field)
-        path, filename = os.path.split(original.name)
-        basename, ext = os.path.splitext(filename)
-        variation_name = '%s_%s_%s%s' % (basename, self.get_shortname(), self.get_options_hash(), ext) #
-        variation_directory = self.variation.file.field.get_directory_name()
-        variation_path = os.path.join(variation_directory, variation_name)
-        storage = get_storage_class()()
 
         # extend the options
         options['functions'][0]['save'] = {
-            'image_identifier' : variation_name,
+            'image_identifier' : self.variation_filename,
             's3_destination' : {
                 'bucket' : settings.AWS_STORAGE_BUCKET_NAME,
-                'key' : os.path.join(storage.location, variation_path)
+                'key' : os.path.join(self.storage.location, self.variation_path)
             }
         }
 
         # extend the options with blitline auth infos and source url
         options.update({
             'application_id' : settings.BLITLINE_APPLICATION_ID,
-            'src' : original.url,
+            'src' : self.original.url,
         })
 
         job_json = simplejson.dumps(options)
@@ -51,7 +43,7 @@ class Generic(Base):
         # if the result is not as expected, this fails.
         self.blitline_job_response = parsed
         self.blitlite_job_id = parsed['results']['job_id']
-        return os.path.join(variation_directory, parsed['results']['images'][0]['image_identifier'])
+        return os.path.join(self.variation_directory, parsed['results']['images'][0]['image_identifier'])
 
     def get_progress(self):
         raw = urllib.urlopen('http://api.blitline.com/poll?job_id=%s' % self.blitlite_job_id).read()
@@ -66,6 +58,6 @@ class Generic(Base):
 
 class Pdf2Jpeg(Generic):
     """
-    pdf2jpeg conversion is simply a noop on blitline
+    pdf2jpeg conversion is simply a noop on blitline and forces .jpg extension
     """
-    pass
+    ext = '.jpg'
--- mediavariations/specs.py
+++ mediavariations/specs.py
@@ -1,15 +1,38 @@
+import os
+
 from django.utils import simplejson
+from django.core.files.storage import get_storage_class
+
 
 class Base(object):
     defaults = {}
 
     def __init__(self, **kwargs):
+        # write down all init args to object attrs -> s = Spec(a=2); s.a -> 2
         for key, value in kwargs.iteritems():
             setattr(self, key, value)
+
+        if hasattr(self, 'variation'):
+            self.original = getattr(self, 'original',
+                getattr(self.variation.content_object, self.variation.field))
+            self.path = getattr(self, 'path', os.path.split(self.original.name)[0])
+            self.filename = getattr(self, 'filename', os.path.split(self.original.name)[1])
+            self.basename = getattr(self, 'basename', os.path.splitext(self.filename)[0])
+            self.ext = getattr(self, 'ext', os.path.splitext(self.filename)[1])
+            self.variation_filename = getattr(self, 'variation_filename',
+                self.get_variation_filename())
+            self.variation_directory = getattr(self, 'variation_directory',
+                self.variation.file.field.get_directory_name())
+            self.variation_path = getattr(self, 'variation_path',
+                os.path.join(self.variation_directory, self.variation_filename))
+            self.storage = getattr(self, 'starage', get_storage_class()())
 
     @classmethod
     def get_shortname(self):
         return self.__name__.lower()
+
+    def get_variation_filename(self):
+        return '%s_%s_%s%s' % (self.basename, self.get_shortname(), self.get_options_hash(), self.ext)
 
     def get_options(self):
         try:
